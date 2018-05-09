cf5eb124163490bee53ae79cecba93de96563528
= <=>
2018-05-09 18:11:50 +0200

    znova som skodil

---
+++ resources/database.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE IF NOT EXISTS L2C_KE_PHP_CMS;
+USE L2C_KE_PHP_CMS;
+
+CREATE TABLE Users (
+	ID Int,
+	email varchar(256),
+	password varchar(64),
+	nickname varchar(128),
+	PRIMARY KEY (ID)
+);
+
+CREATE TABLE Pages (
+	ID int,
+	title varchar(128),
+	content TEXT,
+	User_ID int,
+	menu_label varchar(128),
+	menu_order int,
+	PRIMARY KEY (ID),
+	FOREIGN KEY (User_ID) REFERENCES (Users) ON DELETE CASCADE
+);
